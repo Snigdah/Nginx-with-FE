7ce44b6ba46728042ac2378221aece7bb6a6272e
K M FarhatSnigdah <farhatsnigdho1392@gmail.com>
2026-03-06 19:42:20 +0600

    first commit

---
+++ frontend1/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>THis is FE 1</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend2/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>This is FE 2</h1>
+    </>
+  )
+}
+
+export default App
