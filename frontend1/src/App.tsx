854fc80e7fc2ee0fc5e34cf996f43e37204bd107
K M FarhatSnigdah <farhatsnigdho1392@gmail.com>
2026-03-10 16:41:57 +0600

    added docker file and nginx complete

--- frontend1/src/App.tsx
+++ frontend1/src/App.tsx
@@ -1,12 +1,32 @@
-import './App.css'
+import { useState } from "react";
 
 function App() {
 
+  const [data, setData] = useState(null);
+
+  const callApi = async () => {
+
+    const response = await fetch("/orders/hello");
+
+    const json = await response.json();
+
+    setData(json);
+  };
+
   return (
-    <>
-      <h1>THis is FE 1</h1>
-    </>
-  )
+    <div style={{ padding: "40px" }}>
+      <h1>Gateway Test</h1>
+
+      <button onClick={callApi}>
+        Call Order Service
+      </button>
+
+      {data && (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
+
+    </div>
+  );
 }
 
-export default App
+export default App;
